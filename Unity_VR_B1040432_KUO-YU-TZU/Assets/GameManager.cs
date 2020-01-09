a168c8570eacf844e891062740a8f5500f1495d3
cady27714 <54615342+cady27714@users.noreply.github.com>
2020-01-09 11:36:42 +0800

    0109

---
+++ Unity_VR_B1040432_KUO-YU-TZU/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using Valve.VR;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool haveKey;
+
+    public void ThrowPickUp()
+    {
+        print("撿起物件");
+    }
+
+    public void ThrowDetach(GameObject obj)
+    {
+        print("放開物件");
+        haveKey = true;
+        Destroy(obj);
+    }
+
+    public void ThrowHeld()
+    {
+        print("拿著物件");
+    }
+}
---
+++ Unity_VR_B1040432_KUO-YU-TZU/Assets/TeleportInteratble.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class TeleportInteratble : MonoBehaviour
+{
+
+
+    [Header("動畫控制器")]
+    public Animator animatorControl;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "HeadCollider" && GameManager.haveKey)
+        {
+            animatorControl.SetBool("box", true);
+        }
+    }
+
+}
